950a19f5a1628f644f8143930d997b294a8d8900
Crys <1208344+huncrys@users.noreply.github.com>
2026-02-15 21:21:46 +0000

    chglog: add 0.7.4 bottle.

--- Formula/chglog.rb
+++ Formula/chglog.rb
@@ -6,6 +6,12 @@ class Chglog < Formula
   license "MIT"
   head "https://github.com/goreleaser/chglog.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/huncrys/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "d964c6a87f2076186f9ee6b22eb12858f6707f2c66a8bfc5d278221eb7d4a3ea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1330ebcd369a7216e198e01951fd7f32ad2509915bd02ac0778b74f2b4e71682"
+  end
+
   depends_on "go" => :build
 
   def install
