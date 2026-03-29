80c4afbc62d0c6d3ae8b8467eda89c2677d4ec0a
Crys <1208344+huncrys@users.noreply.github.com>
2026-03-29 10:46:38 +0000

    dtop: update 0.7.2 bottle.

--- Formula/dtop.rb
+++ Formula/dtop.rb
@@ -8,11 +8,11 @@ class Dtop < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/huncrys/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "34f69d932c465ec2debee15e311729f402f985d10c20f7203429da116f9ef59e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3750cbd18420370a4445c0c07f06a1d16b25558fba5b9e987aa5912e42f2c5c4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "748e813b9a1c493aca5e805a3723661ac49473a6945db9ffb8e43575add03b86"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6ddd78e3acc69f4c82a4449c35cfe59f79e17a5ae669dda90d3b34f3dfd837b0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4c28dabfe72c8ac548b52b15e07eeb528ff368979a7f0a1a03fea1b7d4bda174"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1cd8ffaff11b1c3491d82c59b2a2c3ccb071c64176ae841d5c7e42098cecc7e7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "158abcd927efdbe965fa3b3a016ba60dcbb97199bb8ef363fce811d14f7cc615"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9d505b96442af36a6ea68f5c18783c7916df8103fac7d3fb00db3f62ef60b473"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "fce8b93e3e9769f9337f7d778041fbcae7f6bdc370d5f35fa9d328235042fad0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f9dd7a8058abc3c495a9be2e6e52615a9a3f6570698818b35c3a9e243f6630c1"
   end
 
   depends_on "homebrew/core/rust" => :build
